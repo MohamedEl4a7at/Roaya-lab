cc6ea46db34d095aed06b1dc662463dd153e5e48
MohamedEl4a7at <mohamedmustafa93@std.mans.edu.eg>
2023-08-13 21:53:26 +0300

    Update

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 const express                 = require('express'),
       app                     = express(),
-      port                    = process.env.PORT || 3000,
+      port                    = process.env.PORT
       cors                    = require('cors'),
       userRouter              = require('./routers/user')
 
